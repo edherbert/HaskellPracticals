422b092675a0be3e8d67bbfbda88f0c756de148f
edherbert <edwardherbert802@yahoo.com>
2017-11-14 19:11:45 +0000

    Worked on questions 5-7

--- second.hs
+++ second.hs
@@ -26,3 +26,22 @@ sumDiagonalLengths :: Float -> Float -> Float -> Float
 sumDiagonalLengths a b c = pythag a a + pythag b b + pythag c c
 
 --5
+taxiFare :: Int -> Float
+taxiFare distance
+    | distance < 10 = 2.20 + (0.5 * fromIntegral distance)
+    | otherwise = 2.20 + (0.3 * (fromIntegral distance - 10)) + 5
+
+--6
+--howManyAboveAverage :: Int -> Int -> Int -> Int
+--Find the average of the ints first?
+--One could be well above average
+--All of them can't be above the average
+--Check to see if one of them is higher than the others.
+--howManyAboveAverage x y z
+
+--7
+validDate :: Int -> Int -> Bool
+validDate day month
+    | month <= 0 || month > 12 = False
+    | day <= 0 || day > 31 = False
+    | otherwise = True
